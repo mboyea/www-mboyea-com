0facac942d691d1329cf33949e6921f42d672bb9
mboyea <contact@mboyea.com>
2023-08-06 15:19:05 -0500

    added API route; created stage_secrets.sh script; added deploy scripts; updated README

---
+++ src/routes/api/+server.ts
@@ -0,0 +1,16 @@
+import type { ServerLoad } from "@sveltejs/kit";
+
+export const GET: ServerLoad = async ({ params, request }) => {
+}
+
+export const POST: ServerLoad = async ({ params, request }) => {
+}
+
+export const PATCH: ServerLoad = async ({ params, request }) => {
+}
+
+export const PUT: ServerLoad = async ({ params, request }) => {
+}
+
+export const DELETE: ServerLoad = async ({ params, request }) => {
+}
