e10f7a06a2ae19426e665ca4424a7ae46b1ce1f2
Matthew Boyea <contact@mboyea.com>
2023-08-07 18:30:55 -0500

    Database architecture (#6)
    * added API route; created stage_secrets.sh script; added deploy scripts; updated README
    
    * fixed deployment of dependencies with PNPM; created first valid api endpoint for testing; enabled support for comments in .env file; cleaned up gitignore file

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,16 +1,21 @@
-import type { ServerLoad } from "@sveltejs/kit";
+import { json, type ServerLoad } from "@sveltejs/kit";
 
 export const GET: ServerLoad = async ({ params, request }) => {
+	return json('You don\'t have access to this endpoint.');
 }
 
 export const POST: ServerLoad = async ({ params, request }) => {
+	return json('You don\'t have access to this endpoint.');
 }
 
 export const PATCH: ServerLoad = async ({ params, request }) => {
+	return json('You don\'t have access to this endpoint.');
 }
 
 export const PUT: ServerLoad = async ({ params, request }) => {
+	return json('You don\'t have access to this endpoint.');
 }
 
 export const DELETE: ServerLoad = async ({ params, request }) => {
+	return json('You don\'t have access to this endpoint.');
 }
