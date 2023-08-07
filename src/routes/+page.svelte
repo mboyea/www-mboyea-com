39775a8cc74d09d87c6aa016576de6ea66934b21
mboyea <contact@mboyea.com>
2023-08-07 18:28:08 -0500

    fixed deployment of dependencies with PNPM; created first valid api endpoint for testing; enabled support for comments in .env file; cleaned up gitignore file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+	const test = async () => {
+		const request: RequestInit = {
+			method: 'GET',
+		}
+		const response = await fetch('/api', request);
+		const responseJSON = await response.json();
+	}
+	onMount(test);
+</script>
+
 <h1>I'm Matthew Boyea</h1>
 <p>I practice mixed martial arts, fishing, reading, bodybuilding, cooking, running, gymnastics, automotive mechanics, studying (psychology, philosophy, sociology), photography, videography, writing, game development, and software engineering.</p>
