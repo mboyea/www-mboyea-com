d6c794b2c33faf5d38a1e6851867a3c9b58db89d
YourZombieMop <68197922+YourZombieMop@users.noreply.github.com>
2023-10-23 21:15:16 -0500

    fixed package.json file path

--- scripts/deploy_database.sh
+++ scripts/deploy_database.sh
@@ -7,7 +7,7 @@ set -a; source .env; set +a
 flyctl proxy 5432 -a mboyea-database-test & proxy_program="$!"
 
 # standup database
-PGPASSWORD=$PG_PASSWORD psql -p 5432 -U postgres -f src/sql/standup_database.sql
+PGPASSWORD=$PG_PASSWORD psql -p 5432 -U postgres -f scripts/sql/standup_database.sql
 
 # kill proxy
 kill $proxy_program
