9a777cb571b0c21e47431062af63067368d13adc
Matthew Boyea <contact@mboyea.com>
2023-10-25 19:37:23 -0500

    Feature/sql query (#13)
    * added Article Table; implemented Article read query; created sql database connection test in psql.ts
    
    * added Article TS type; added error handling to Article/id query; made homepage image more blurry
    
    * improved exception handling of GET article/id

--- src/lib/client/api/ArticleRequests.ts
+++ src/lib/client/api/ArticleRequests.ts
@@ -1,4 +1,6 @@
-// get all articles
+import type { Article } from "$types/Article";
+
+/** Get all articles. */
 export const getArticles = async () => {
 	const request: RequestInit = {
 		method: 'GET',
@@ -8,12 +10,21 @@ export const getArticles = async () => {
 	return response.json();
 }
 
-// get article by ID
-export const getArticle = async (id: string) => {
+/** Get article by ID. */
+export const getArticle = async (id: number): Promise<Article> => {
+	const route = `/api/v1/articles/${id}`;
 	const request: RequestInit = {
 		method: 'GET',
 		redirect: 'follow',
 	}
-	const response = await fetch(`/api/v1/articles/${id}`, request);
-	return response.json();
+	const response = await fetch(route, request);
+	if (!response.ok) {
+		throw new Error(
+			`fetch route: ${route}\n` +
+			`response: ${response.status} ${response.statusText}\n` +
+			`payload: ${await response.json()}`
+		);
+	}
+	const result: Article = await (response.json() as Promise<Article>);
+	return result;
 }
--- src/routes/api/v1/articles/[slug]/+server.ts
+++ src/routes/api/v1/articles/[slug]/+server.ts
@@ -2,15 +2,25 @@ import { json, type ServerLoad } from "@sveltejs/kit";
 import psql from "$lib/server/utils/psql";
 
 export const GET: ServerLoad = async ({ params, request }) => {
-	//TODO: return article from database by SLUG
-
-	/*psql.connect();
-	const res = await psql.query('SELECT $1::text as message', ['Hello world!'])
-	console.log(res.rows[0].message)
-	psql.end();
-
-	return json(res.rows[0].message);*/
-	return json('');
+	if (isNaN(parseInt(params.slug || 'a'))) {
+		return json('Ivalid article ID.', { status: 404 });
+	}
+	const response = await psql.query(`
+		SELECT
+			id,
+			title,
+			publish_date AS publishDate,
+			last_edit_date AS lastEditDate,
+			description_md AS descriptionMd,
+			summary_md AS summaryMd,
+			text_md AS textMd
+		FROM article
+		WHERE id=${params.slug}
+	`);
+	if (response.rowCount < 1) {
+		return json('Article not found.', { status: 404 });
+	}
+	return json(response.rows[0]);
 }
 
 export const POST: ServerLoad = async ({ params, request }) => {
