535ca73b4a51da15eec78fc5c3fd847c144b6a16
YourZombieMop <68197922+YourZombieMop@users.noreply.github.com>
2023-11-16 00:03:31 -0600

    Article data is queried server-side so article data is shown at-first-paint for SEO benefits; style - articles now lengthen to 100% of available screen height

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -1,43 +1,40 @@
 <script lang="ts">
 	import { marked } from 'marked';
-	import { getArticle } from "$api/ArticleRequests";
   import type { PageData } from "./$types";
 	export let data: PageData;
-	const articleData = getArticle(data.slug);
 </script>
 
-{#await articleData}
-<p>Loading article...</p>
-{:then article}
+{#if data.article}
 <article>
 	<header>
-		<h1>{article.title}</h1>
-		<p>Published: {article.publishDate}</p>
-		{#if article.publishDate != article.lastEditDate}
-			<p>Last Edited: {article.lastEditDate}</p>
+		<h1>{data.article.title}</h1>
+		<p>Published: {data.article.publishDate}</p>
+		{#if data.article.publishDate != data.article.lastEditDate}
+			<p>Last Edited: {data.article.lastEditDate}</p>
 		{/if}
 	</header>
-	{#if article.descriptionMd}
+	{#if data.article.descriptionMd}
 		<section class="description">
 			<h2>Description</h2>
-			{@html marked.parse(article.descriptionMd)}
+			{@html marked.parse(data.article.descriptionMd)}
 		</section>
 	{/if}
-	{#if article.summaryMd}
+	{#if data.article.summaryMd}
 		<section class="summary">
 			<h2>Summary</h2>
-			{@html marked.parse(article.summaryMd)}
+			{@html marked.parse(data.article.summaryMd)}
 		</section>
 	{/if}
-	{@html marked.parse(article.textMd)}
+	{@html marked.parse(data.article.textMd)}
 </article>
-{:catch error}
-<p>{error}</p>
-{/await}
+{:else if data.articleError}
+<p>{data.articleError}</p>
+{/if}
 
 <style lang="scss">
 	article {
 		width: min(100%, 54rem);
+		height: 100%;
 		margin: 0 auto;
 		padding: 1rem 2rem;
 		background-color: var(--article-background-color);
