9d01090ea4176374341ce0280369ec5f662480ec
YourZombieMop <68197922+YourZombieMop@users.noreply.github.com>
2023-11-25 17:15:58 -0600

    added blurb to homepage for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <h1 id="featured-img">I'm Matthew Boyea.</h1>
+<p>My deepest motivation is to be an excellent father. I am well-read in philosophy, psychology, and sociology purely for this purpose - to be able to raise amazing young men and women effectively, without subjecting them to trauma. The goal is to facilitate a capacity for self-control which enables them to live without fear, guilt, and shame. This is the culture I have created in my own mind. If I were to fail this goal, I would love my kids unconditionally and feel no disappointment in them or myself. I hope to adopt children in need and offer them a place in my family.</p>
+<p>I intend to make a lot of money using my understanding of people, communication, and business. I intend to design and sell many great products with my skills and understanding of software, hardware, mechanics., and people These lovely and fulfilling endeavors of my life will fund my investment into my children and family. Once I am settled in my own home, I expect that I'll start searching the planet for a partner with complimentary culture and create the family of my dreams.</p>
+<p>I practice many hobbies and endeavor to be as skilled as I am able in the time I have for them. Most of my time right now is spent towards my relationships (meeting with family and friends), doing physical conditioning (weightlifting, MMA practice), and preparing for my career (job applications, portfolio work). I practice mixed martial arts, fishing, reading, bodybuilding, cooking, running, gymnastics, automotive mechanics, studying, photography, videography, writing, game development, and software engineering.</p>
 <p>TODO: /home styles</p>
-<p>I practice mixed martial arts, fishing, reading, bodybuilding, cooking, running, gymnastics, automotive mechanics, studying (psychology, philosophy, sociology), photography, videography, writing, game development, and software engineering.</p>
 
 <style lang="scss">
 	#featured-img {
