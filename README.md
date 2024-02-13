b5e6e42f7046474d36db86b590e99397533fed30
Matthew Boyea <contact@mboyea.com>
2024-02-13 00:21:14 -0600

    added commands start & article to package.json & readme; changed command dev to start:dev; changed command preview to start:preview; changed script names to dash-case from underscore_case; created outline of scripts/article.sh

--- README.md
+++ README.md
@@ -33,14 +33,16 @@ To run a script, type `pnpm run <script-name>` into a terminal within the root f
 
 | script-name | description |
 |:----------- |:----------- |
-| `dev` | create a local hot-reloading server at [localhost:5173](http://localhost:5173) for development purposes |
-| `preview` | build app using .env.development, create a local server at [localhost:4173](http://localhost:4173) |
+| `start` | run start:dev |
+| `start:dev` | create a local hot-reloading server at [localhost:5173](http://localhost:5173) for development purposes |
+| `start:preview` | build app using .env.development, create a local server at [localhost:4173](http://localhost:4173) |
 | `deploy` | update app dependencies, build app, deploy database, deploy secrets, deploy server |
 | `deploy:database` | update postgres users, databases, tables, procedures, etc. |
 | `deploy:secrets` | set flyctl secrets from .env file |
 | `deploy:server` | deploy build folder to flyctl |
 | `check` | evaluate Svelte syntax |
 | `check:watch` | re-evaluate Svelte syntax when files are updated |
+| `article` | cli to modify articles in the postgres database; pass -h for more details |
 
 ### Create Secrets
 Some features of this app require secret database credentials. These secrets cannot be shared in public.
---
+++ scripts/article.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+help() {
+	echo "Use a secure tunnel to access or modify articles stored in the postgres databse from the command line"
+	echo
+	echo "Usage: article <url> [Option]..."
+	echo
+	echo "Options:"
+	echo "  ACTION OPTIONS"
+	echo "  -h         display this help & exit"
+	echo "  -u         upload data to URL & exit"
+	echo "  DATA OPTIONS"
+	echo "  -d <file>  description target file"
+	echo "  -s <file>  summary target file"
+	echo "  -T <file>  title target file"
+	echo "  -t <file>  text target file"
+	echo
+}
+
+OPTIND=1
+while getopts ":T:t:d:s:" data; do
+	case $data in
+		d)
+			;;
+		s)
+			;;
+		T)
+			;;
+		t)
+			;;
+	esac
+done
+
+OPTIND=1
+while getopts ":hu" action; do
+	case $action in
+		h)
+			help
+			exit
+			;;
+		u)
+			echo "Feature not yet implemented."
+			echo
+			exit
+			;;
+	esac
+done
+
+
+#TODO: given input files for article title, text, description, summary, etc as command line arguments upload the article into the database
+
+
+# create proxy to connect to database on port 5432
+# flyctl proxy 5432 -a mboyea-database-test & proxy_program="$!"
+
+# standup database
+# PGPASSWORD=$PG_PASSWORD psql -h localhost -p 5432 -U postgres -d postgres -f scripts/sql/main_deploy.psql
+
+# kill proxy
+# kill $proxy_program
+
+echo "Invalid command. Use -h for help with usage."
+echo
+
