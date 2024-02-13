110e0b61a35af28d7466fe7f8b1cc64d505afd19
Matthew Boyea <contact@mboyea.com>
2024-02-13 15:46:03 -0600

    added cleanup to start-server scripts

--- scripts/start-dev-server.sh
+++ scripts/start-dev-server.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+cleanup() {
+	# kill proxy
+	kill $!
+}
+
+# trap exit of script and call cleanup()
+trap cleanup EXIT
+
 # create proxy to connect to database on port 5432
 flyctl proxy 5432 -a mboyea-database-test &
 
 # start dev server
 vite dev
 
-# kill proxy
-kill $!
--- scripts/start-preview-server.sh
+++ scripts/start-preview-server.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+cleanup() {
+	# kill proxy
+	kill $!
+}
+
+# trap exit of script and call cleanup()
+trap cleanup EXIT
+
 # create proxy to connect to database on port 5432
 flyctl proxy 5432 -a mboyea-database-test &
 
 # start preview server
 vite preview
 
-# kill proxy
-kill $!
