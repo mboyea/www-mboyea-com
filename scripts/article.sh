560177c687f8b4fbe2c7d07357100fe49aaf9007
Matthew Boyea <contact@mboyea.com>
2024-02-13 23:58:05 -0600

    implemented article script proxy; implemented article script .env loading; outlined article script upload function; implemented options reading

--- scripts/article.sh
+++ scripts/article.sh
@@ -8,7 +8,9 @@ usage() {
 help() {
 	echo "Use a secure tunnel to access or modify articles stored in the postgres databse from the command line"
 	echo
+
 	usage
+
 	echo "Options:"
 	echo "  ACTION OPTIONS"
 	echo "  -h         display this help & exit"
@@ -21,16 +23,59 @@ help() {
 	echo
 }
 
+create_proxy() {
+	# prepare proxy program cleanup
+	cleanup() {
+		# kill proxy
+		kill -INT "$proxy_program_PID"
+		sleep 0.4
+	}
+	trap cleanup EXIT
+	# create proxy to connect to database on port 5432
+	coproc proxy_program (flyctl proxy 5432 -a mboyea-database-test)
+	# wait for output from proxy program
+	read output <&"${proxy_program[0]}"
+	echo $output
+}
+
+load_env() {
+	# load .env
+	set -a; source .env; set +a
+}
+
+upload() {
+	# TODO: pass -c "" code w/ INSERT statement
+	PGPASSWORD=$PG_PASSWORD psql -h localhost -p 5432 -U postgres -d mboyea_main #-c ""
+	# PGPASSWORD=$PG_PASSWORD psql -h localhost -p 5432 -U postgres -d postgres -f scripts/sql/main_deploy.psql
+	echo "$description $summary $title $text"
+	# https://stackoverflow.com/questions/3953645/ternary-operator-in-bash
+
+	# /*
+	# INSERT INTO article (title, url, description_md, summary_md, text_md)
+	# VALUES (
+	# 	'Mock Article',
+	# 	'mock-article',
+	# 	E'This is a brief article to be used for testing.',
+	# 	E'In summary:\n> all functionality should be working!\n\n- *Lists*\n- **Text decoration**\n- `Code snippets`',
+	# 	E'## Headers should work.\n### H3\n#### H4\n##### H5\n###### H6\n## Blockquotes should be functional.\n> This way, we can draw attention to important words.\n\n## Code blocks should also work.\n```cpp\nint main() {\nstd::cout << "Hello world!";\n}\n```'
+	# );
+	# */
+}
+
 OPTIND=1
-while getopts ":T:t:d:s:" data; do
+while getopts ":d:s:T:t:" data; do
 	case $data in
 		d)
+			description="$OPTARG"
 			;;
 		s)
+			summary="$OPTARG"
 			;;
 		T)
+			title="$OPTARG"
 			;;
 		t)
+			text="$OPTARG"
 			;;
 	esac
 done
@@ -43,25 +88,13 @@ while getopts ":hu" action; do
 			exit
 			;;
 		u)
-			echo "Feature not yet implemented."
-			echo
+			create_proxy
+			load_env
+			upload
 			exit
 			;;
 	esac
 done
-
-
-#TODO: given input files for article title, text, description, summary, etc as command line arguments upload the article into the database
-
-
-# create proxy to connect to database on port 5432
-# flyctl proxy 5432 -a mboyea-database-test & proxy_program="$!"
-
-# standup database
-# PGPASSWORD=$PG_PASSWORD psql -h localhost -p 5432 -U postgres -d postgres -f scripts/sql/main_deploy.psql
-
-# kill proxy
-# kill $proxy_program
 
 usage
 
